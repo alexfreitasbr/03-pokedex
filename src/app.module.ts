08dcace81a884e3217ecd886752553314f1d4cd3
alex freitas <alexfreitasbr@hotmail.com>
2026-05-13 13:48:25 -0300

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule} from '@nestjs/mongoose'
 
 @Module({
   imports: [
@@ -10,9 +10,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI ?? 'mongodb://localhost:27017/nest-pokemon',
-    ),
+    MongooseModule,
 
     PokemonModule,
   ],
